29b5e07eb89fbde7657e16669e41168486292bf0
Futsum Nemariam <futsumnemariam2@gmil.com>
2020-10-16 14:32:53 -0700

    Updates

---
+++ precache-manifest.6f8ba569fe08d825c9f314052c6a2c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6766a7878f91c5e537aee7faf871efd0",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "fd47030fd5cf3e337b11",
+    "url": "/project-4-assessment/static/css/main.ab726b44.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd47030fd5cf3e337b11",
+    "url": "/project-4-assessment/static/js/main.93682921.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
